24a3ae3a0e0fe9bb8ec71554604d242e57251200
Schow94 <Schow94@gmail.com>
2019-10-10 20:34:53 -0700

    Updates

---
+++ precache-manifest.9c58930c94e38bce46de7f2e574b12e4.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d642df3f1b86421edef17bd705bee8a6",
+    "url": "/Todos-hooks/index.html"
+  },
+  {
+    "revision": "d26397d791abc6c2e8c6",
+    "url": "/Todos-hooks/static/js/2.d224efda.chunk.js"
+  },
+  {
+    "revision": "b795b26746b493900255",
+    "url": "/Todos-hooks/static/js/main.c1e281db.chunk.js"
+  },
+  {
+    "revision": "03d33c715f78aadf9ae6",
+    "url": "/Todos-hooks/static/js/runtime~main.a04dd1b9.js"
+  }
+]);
